5b2b7b453ea33473542e2af662ccf07cf6933e4e
Konrad Botor <kbotor@syncad.com>
2023-07-06 13:45:27 +0200

    Added backend Dockerfile and Bakefile, updated Composefiles - ref. #8

--- scripts/setup_db.sh
+++ scripts/setup_db.sh
@@ -6,6 +6,7 @@ POSTGRES_USER=${POSTGRES_USER:-"haf_admin"}
 POSTGRES_HOST=${POSTGRES_HOST:-"localhost"}
 POSTGRES_PORT=${POSTGRES_PORT:-5432}
 POSTGRES_URL=${POSTGRES_URL:-""}
+SKIP_IF_DB_EXISTS=${SKIP_IF_DB_EXISTS:-"false"}
 
 while [ $# -gt 0 ]; do
   case "$1" in
@@ -20,6 +21,12 @@ while [ $# -gt 0 ]; do
         ;;
     --postgres-url=*)
         POSTGRES_URL="${1#*=}"
+        ;;
+    --skip-if-db-exists=*)
+        SKIP_IF_DB_EXISTS="${1#*=}"
+        ;;
+    --skip-if-db-exists)
+        SKIP_IF_DB_EXISTS="true"
         ;;
     -*)
         echo "ERROR: '$1' is not a valid option"
@@ -36,6 +43,12 @@ while [ $# -gt 0 ]; do
 done
 
 POSTGRES_ACCESS=${POSTGRES_URL:-"postgresql://$POSTGRES_USER@$POSTGRES_HOST:$POSTGRES_PORT/haf_block_log"}
+
+if [[ "$SKIP_IF_DB_EXISTS" == "true" ]]; then
+    psql "$POSTGRES_ACCESS" -c "SELECT 'btracker_app.main'::regproc"
+    DB_EXISTS=$?
+    [[ "$DB_EXISTS" == "0" ]] && echo "Database already exists, exiting..." && exit 0
+fi
 
 echo "Installing app..."
 psql "$POSTGRES_ACCESS" -v ON_ERROR_STOP=on -f "$SCRIPTPATH/../db/builtin_roles.sql"
